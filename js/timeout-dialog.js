706958a3c033c71d6a703928cfbf0f913134d5a0
Derek Lords <lords@ohsu.edu>
2023-05-16 13:53:35 -0700

    try password blank?

--- js/timeout-dialog.js
+++ js/timeout-dialog.js
@@ -250,12 +250,12 @@
               open: function(event, ui) {
                 $('.ui-widget-overlay').css({ opacity: '.98' });
                 $(this).dialog('option', 'title', settings.endTitle);
+                $("#password").val('');
                 $("#password").removeAttribute('readonly');
               },
               close: function(event, ui) {
                 $(this).dialog('destroy').remove();
               }
-
             }); 
         }
       }
